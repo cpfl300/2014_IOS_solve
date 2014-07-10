b634f0baec92f0e0928531a24b4e402ac53eee54
cpfl300 <cpfl300@naver.com>
2014-07-10 16:19:23 +0900

    file Matcher

--- week1/week1/main.m
+++ week1/week1/main.m
@@ -18,6 +18,32 @@ void printAllSubFiles(NSString *path){
     
 }
 
+NSMutableArray* findAllFileToArray(NSString *path){
+    NSMutableArray* files = [[NSMutableArray alloc] init];
+    
+    NSFileManager *manager = [[NSFileManager alloc] init];
+    NSDirectoryEnumerator *fileEnumerator = [manager enumeratorAtPath:path];
+    
+    for(NSString *filename in fileEnumerator){
+        [files addObject:filename];
+    }
+    
+    return files;
+}
+
+void isExistFilename(NSString* findfile, NSString* path){
+    NSFileManager *manager = [[NSFileManager alloc] init];
+    NSDirectoryEnumerator *fileEnumerator = [manager enumeratorAtPath:path];
+    
+    for(NSString *filename in fileEnumerator){
+        if([filename isEqualToString:findfile] == 1){
+            NSLog(@"FIND \"%@\" file!! \n",filename);
+            return;
+        }
+    }
+    NSLog(@"I DON'T HAVE \"%@\" file...\n",findfile);
+}
+
 int main(int argc, const char * argv[])
 {
 
@@ -27,7 +53,16 @@ int main(int argc, const char * argv[])
         NSURL *myURL = [NSURL URLWithString:
                         @"file:////Users/Kimminju/Documents/2-2NEXT/IOS"];
         NSString *path = [myURL path];
-        printAllSubFiles(path);
+//        printAllSubFiles(path);
+        
+//        NSArray* temp = findAllFileToArray(path);
+        
+        
+        NSString* temp = @"2014_IOS_solve";
+        isExistFilename(temp, path);
+
+        
+
     }
     return 0;
 }
